8e3fd374c56f2f4f4747b87f98e11233057ca4b1
deyvis0n <deyvisonsferreira@outlook.com>
2021-12-11 22:46:20 -0300

    correção na home

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -1,10 +1,17 @@
+import { useState } from 'react'
+import AuthService from '../../services/auth.service'
+
 export default function Home() {
+    const [currentUser] = useState(AuthService.getCurrentUser)
+
     return(
         <div className= 'home container'>
             <h1 className='home-titulo'>Diário Escolar de Sentimentos</h1>
             <p className='home home-subtitulo'>Este diário é direcionado aos alunos do ETE Ginásio Pernambucano, onde os alunos podem expressar seus sentimentos anonimamente, com o intuito da instituição de ensino promover atividades socioemocionais.
             </p>
-            <a className='btn btn-secondary btn-home btn-lg' href='/register' role='button'>Cadastre-se</a>
+            { !currentUser &&
+                 <a className='btn btn-secondary btn-home btn-lg' href='/register' role='button'>Cadastre-se</a>
+            }
         </div>
     )
 }
